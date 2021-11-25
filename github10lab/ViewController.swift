5852aac9b231ae5fd6132f6351e0c7271436e6ce
OtzhanAigerim <seai01@mail.ru>
2021-11-26 01:37:02 +0600

    Initial Commit

---
+++ github10lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github10lab
+//
+//  Created by Aigerima Otzhan on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
